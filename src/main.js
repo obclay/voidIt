07cfe999e83348cbc7443338920fe906e480d668
Olivia Clay <oliviabclay@gmail.com>
2019-04-01 21:26:12 -0400

    update package.json

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
 
@@ -8,9 +8,9 @@ import './global.css';
 import './skeletons.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js');
